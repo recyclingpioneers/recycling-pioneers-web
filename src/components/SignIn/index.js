b5a9ee0fb2c93c7d6f098deb30d471c30b06207b
arneetsinghkalra <a.s.kalra1999@gmail.com>
2020-07-20 16:10:59 -0400

    Initial Commit

---
+++ src/components/SignIn/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignIn = () => (
+  <div>
+    <h1>Sign in</h1>
+  </div>
+);
+
+export default SignIn;
---
+++ src/components/SignUp/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignUp = () => (
+  <div>
+    <h1>Sign up</h1>
+  </div>
+);
+
+export default SignUp;
