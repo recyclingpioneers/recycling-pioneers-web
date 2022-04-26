fd23773c7d2c8b4c4fbe3c917b0cc2ede4084667
arneetsinghkalra <a.s.kalra1999@gmail.com>
2022-04-25 20:10:59 -0400

    Added alex and yifan

--- src/components/About/TeamMemberData.jsx
+++ src/components/About/TeamMemberData.jsx
@@ -1,46 +1,59 @@
-import arneet from "../../assets/headshot_arneet.png";
-import misghana from "../../assets/headshot_misghana.png";
-import simina from "../../assets/headshot_simina.png";
-import kirk from "../../assets/headshot_kirk.png";
-import yann from "../../assets/headshot_yann.png";
-import prabhsimran from "../../assets/headshot_prabhsimran.png";
+import arneet from "../../assets/headshots/headshot_arneet.png";
+import misghana from "../../assets/headshots/headshot_misghana.png";
+import simina from "../../assets/headshots/headshot_simina.png";
+import kirk from "../../assets/headshots/headshot_kirk.png";
+import yann from "../../assets/headshots/headshot_yann.png";
+import prabhsimran from "../../assets/headshots/headshot_prabhsimran.png";
+import missing from "../../assets/headshots/headshot_unknown.jpg";
 
 const MemberInfo = [
   {
     imgsrc: kirk,
     name: "Kirk Lau",
-    summary: "PhD Student in Materials Engineering at McGill",
+    summary: "PhD Student in Materials Engineering",
     role: "Co-founder: Administration"
   },
   {
     imgsrc: misghana,
     name: "Misghana Kassa",
-    summary: "B. Sc Life Sciences from McGill",
+    summary: "B. Sc Life Sciences",
     role: "Co-founder: Marketing and Public Outreach"
   },
   {
     imgsrc: simina,
     name: "Simina Alungulesa",
-    summary: "Master's Student in Chemical Engineering at McGill",
+    summary: "Masters in Chemical Engineering",
     role: "Co-founder: R&D"
   },
   {
     imgsrc: arneet,
     name: "Arneet Singh Kalra",
-    summary: "B.Eng Software Engineering Student at McGill University",
-    role: "Software Engineer"
+    summary: "B.Eng Software Engineering",
+    role: "Lead Software Engineer"
   },
   {
     imgsrc: prabhsimran,
     name: "Prabhsimran Singh",
-    summary: "Master's Student in Software Engineering at McGill",
+    summary: "Master's Student in Software Engineering",
     role: "Software Engineer"
   },
   {
     imgsrc: yann,
     name: "Yann Bonzom",
-    summary: "BSc Student in Computer Science at McGill",
+    summary: "BSc Student in Computer Science",
     role: "Software Engineer"
+  },
+  {
+    imgsrc: missing,
+    name: "Alex Wang",
+    summary: "Mechanical Engineering",
+    role: "Hardware Engineer"
+  },
+  {
+    imgsrc: missing,
+    name: "Yifan Wu",
+    summary: "French English Translation",
+    role: "Content Creator"
   }
 ];
 
